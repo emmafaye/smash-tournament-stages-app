e19089e44c6f9592cfe4134f0f61ea1a0405265e
Emma Faye <emzfaye@gmail.com>
2019-07-29 18:53:34 -0700

    MZS Stages - WebApp

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# emmafaye.github.io
+# Smash Stages
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/emmafaye/pen/PrXBgX](https://codepen.io/emmafaye/pen/PrXBgX).
+
+ 
---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+	console.log('Load');
+	let stages = document.getElementById('stages').children;
+	
+	for (let child of stages) {
+		child.addEventListener('click', (event) => {
+			let childClasses = child.classList;
+
+			child.classList.toggle('banned-stage');
+		});
+	}
+});
